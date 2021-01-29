36be03e198bc2fb9be8c92d938bc05f58f8d33d6
TranNgocSon1996 <transon821996@gmail.com>
2021-01-29 13:08:30 +0700

    [ADD] viin_brand_common: add module change theme for Viindoo Branding

---
+++ viin_brand_common/__manifest__.py
@@ -0,0 +1,52 @@
+{
+    'name': "Viindoo Branding Common",
+    'name_vi_VN': "Thiết kế với thương hiệu Viindoo",
+
+    'summary': """
+Frontend theme for Viindoo Branding
+""",
+
+    'summary_vi_VN': """
+Chủ đề giao diện cho thương hiệu Viindoo
+        """,
+
+    'description': """
+This module change some information for Viindoo branding
+ 
+Editions Supported
+==================
+1. Community Edition
+2. Enterprise Edition
+
+    """,
+
+    'description_vi_VN': """
+Mô đun này thay đổi một vài thông tin dành riêng cho thương hiệu Viindoo
+
+Ấn bản được Hỗ trợ
+==================
+1. Ấn bản Community
+2. Ấn bản Enterprise
+
+    """,
+
+    'author': "Viindoo",
+    'website': " https://viindoo.com",
+    'live_test_url': "https://v13demo-int.erponline.vn",
+    'support': "apps.support@viindoo.com",
+    'category': 'Hidden',
+    'version': '0.1',
+    'depends': ['website'],
+    'images' : [
+        'static/description/icon.png'
+    ],
+    'data' : [
+        'views/assets.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'price': 99.9,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+}
