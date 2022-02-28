ccea55a8c5efc5b7d296886de9125888c3981287
Louis <hididaf120@peykesabz.com>
2022-02-28 16:18:52 +0700

    [ADD] viin_brand_website_blog: Improve blog post name is heading

---
+++ viin_brand_website_blog/__manifest__.py
@@ -0,0 +1,53 @@
+{
+    'name': "Website Blog - Heading blog post name for Viindoo",
+    'name_vi_VN': "",
+
+    'summary': """
+Improve blog post name is heading""",
+
+    'summary_vi_VN': """
+Cải tiến Tiêu đề bài viết là thẻ Heading
+        """,
+
+    'description': """
+
+Editions Supported
+==================
+1. Community Edition
+
+    """,
+
+    'description_vi_VN': """
+
+Ấn bản được Hỗ trợ
+==================
+1. Ấn bản Community
+
+    """,
+
+    'author': "Viindoo",
+    'website': "https://viindoo.com",
+    'live_test_url': "https://v13demo-int.erponline.vn",
+    'support': "apps.support@viindoo.com",
+    'category': 'Hidden',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['website_blog'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/website_blog_templates.xml',
+    ],
+
+    'images': [
+        # 'static/description/main_screenshot.png'
+        ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+    'price': 0.0,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+}
