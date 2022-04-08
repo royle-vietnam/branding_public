93da1e63c67e32295224a54f860689246f9d2fe2
Nguyen Hong Quang <nguyenquang2796@gmail.com>
2022-04-08 14:29:36 +0700

    [ADD] viin_brand_website_event_track: redirect to Viindoo

---
+++ viin_brand_website_event_track/__manifest__.py
@@ -0,0 +1,62 @@
+{
+    'name': "Advanced Events Branding For Viindoo",
+    'name_vi_VN': "Giao diện Viindoo cho module Advanced Events",
+
+    'summary': """
+Theme branding Viindoo for module Advanced Events""",
+    'summary_vi_VN': """
+Giao diện brand Viindoo cho module Advanced Events
+""",
+
+    'description': """
+What it does
+============
+This module will change color in navigate bar, button and logo,v.v module Advanced Events following Viindoo's brand
+
+
+Editions Supported
+==================
+1. Community Edition
+2. Enterprise Edition
+
+    """,
+
+    'description_vi_VN': """
+Ứng dụng này làm gì
+===================
+Module này sẽ thay đổi giao diện module Advanced Events theo thương hiệu Viindoo
+
+
+Ấn bản được Hỗ trợ
+==================
+1. Ấn bản Community
+2. Ấn bản Enterprise
+
+""",
+
+    'author': "Viindoo",
+    'website': "https://viindoo.com",
+    'live_test_url': "https://v14demo-int.viindoo.com",
+    'live_test_url_vi_VN': "https://v14demo-vn.viindoo.com",
+    'support': "apps.support@viindoo.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Hidden',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['website_event_track'],
+
+    # always loaded
+    'data': [
+        'views/event_sponsor_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+    'price': 0.0,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+}
