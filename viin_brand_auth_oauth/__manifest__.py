439315b4f573518a56192fa6b936157bbbb071ef
David Tran <david.tran@tvtmarine.com>
2022-04-20 11:15:07 +0700

    [MISC] *: installable False for newly v14-added modules

--- viin_brand_auth_oauth/__manifest__.py
+++ viin_brand_auth_oauth/__manifest__.py
@@ -53,9 +53,9 @@ Module này sẽ thay đổi giao diện module OAuth2 Authentication theo thư�
     'data': [
         'views/res_config_settings_views.xml',
     ],
-    'installable': True,
+    'installable': False,
     'application': False,
-    'auto_install': True,
+    'auto_install': False, # Set True after upgrading for v15
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
--- viin_brand_website_event/__manifest__.py
+++ viin_brand_website_event/__manifest__.py
@@ -53,9 +53,9 @@ Module này sẽ thay đổi giao diện module Events theo thương hiệu Viin
     'demo': [
         'data/event_demo.xml',
     ],
-    'installable': True,
+    'installable': False,
     'application': False,
-    'auto_install': True,
+    'auto_install': False, # Set True after upgrading for v15
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
