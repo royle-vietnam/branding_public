f9379752f42b903145d21d103ac091318c217140
Nguyen Hong Quang <nguyenquang2796@gmail.com>
2022-06-10 11:12:15 +0700

    [IMP]viin_brand_website: using `replace` instead of conditional to cover the scenario Dialog's Title contains one or more `Odoo` keywords

--- viin_brand_website/static/src/js/dialog.js
+++ viin_brand_website/static/src/js/dialog.js
@@ -1,14 +1,12 @@
 odoo.define('viin_brand_website.Dialog', function(require) {
 	"use strict";
 	var Dialog = require('web.Dialog');
-	var core = require('web.core');
-	var _t = core._t;
 
 	Dialog.include({
 
 		init: function (parent, options) {
 			this._super(...arguments);
-			this.title = this.title == _t('Odoo') ? _t('Viindoo') : this.title;
+			this.title = this.title.replace(/Odoo/g,'Viindoo');
 	    }
 	});
 	return Dialog;
