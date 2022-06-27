ccde5ae7a8864bb036ab8cd1df21c830b6a6fbc5
thanhbinh <thanhbinh@thanhbinh-GL552VX>
2022-06-27 09:20:40 +0700

    [UPG] viin_brand_digest: Upgrade module to 15.0

--- viin_brand_digest/__manifest__.py
+++ viin_brand_digest/__manifest__.py
@@ -53,9 +53,9 @@ Module này sẽ thay đổi giao diện module Digest theo thương hiệu Viin
     'data': [
         'data/digest_data.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
-    'auto_install': False, # Set True after upgrading for v15
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
