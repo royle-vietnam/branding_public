aabe4bc9818e4ebc0a072c57e53753494a5fd2ca
Nguyen Hong Quang <nguyenquang2796@gmail.com>
2022-07-06 15:54:58 +0700

    [UPG] viin_brand_payment_paypal: upgrade to 15.0

--- viin_brand_payment_paypal/__manifest__.py
+++ viin_brand_payment_paypal/__manifest__.py
@@ -41,7 +41,7 @@ Module này sẽ thay đổi giao diện các module Paypal Payment Acquirer the
     'support': "apps.support@viindoo.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Hidden',
     'version': '0.1',
@@ -53,9 +53,9 @@ Module này sẽ thay đổi giao diện các module Paypal Payment Acquirer the
     'data': [
         'views/payment_views.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
-    'auto_install': False, # Set True after upgrading for v15
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
