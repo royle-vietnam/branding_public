980bcf12f7f9afd01954ccb132e88b75d37cd89f
hung <hungvuhunglinh@gmail.com>
2022-07-07 08:45:40 +0700

    [UPG]viin_brand_iap: upgrade to 15.0

--- viin_brand_iap/__manifest__.py
+++ viin_brand_iap/__manifest__.py
@@ -53,9 +53,9 @@ Module này sẽ thay đổi giao diện module In-App Purchases theo thương h
     'data': [
         'views/res_config_settings.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
-    'auto_install': False, # Set True after upgrading for v15
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
