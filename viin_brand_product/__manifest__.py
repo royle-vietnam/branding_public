9f28a28e892fa9ea6011562c70046f46b43fd828
hung <hungvuhunglinh@gmail.com>
2022-07-28 16:43:09 +0700

    [IMP]viin_brand*: Change link document to Viindoo document

--- viin_brand_product/__manifest__.py
+++ viin_brand_product/__manifest__.py
@@ -1,17 +1,17 @@
 {
-    'name': "Two-Factor Authentication Branding For Viindoo",
-    'name_vi_VN': "Giao diện Viindoo cho module Two-Factor Authentication",
+    'name': "Product Branding For Viindoo",
+    'name_vi_VN': "Giao diện Viindoo cho module Product",
 
     'summary': """
-Theme branding Viindoo for module Two-Factor Authentication""",
+Theme branding Viindoo for module Product""",
     'summary_vi_VN': """
-Giao diện brand Viindoo cho module Two-Factor Authentication
+Giao diện brand Viindoo cho module Product
 """,
 
     'description': """
 What it does
 ============
-This module will change color navigate bar, button and logo,v.v in module Two-Factor Authentication following Viindoo's brand
+This module will change color navigate bar, button and logo,v.v in Product following Viindoo's brand
 
 
 Editions Supported
@@ -24,7 +24,7 @@ Editions Supported
     'description_vi_VN': """
 Ứng dụng này làm gì
 ===================
-Module này sẽ thay đổi giao diện module Two-Factor Authentication theo thương hiệu Viindoo
+Module này sẽ thay đổi giao diện các module Product theo thương hiệu Viindoo
 
 
 Ấn bản được Hỗ trợ
@@ -36,23 +36,22 @@ Module này sẽ thay đổi giao diện module Two-Factor Authentication theo t
 
     'author': "Viindoo",
     'website': "https://viindoo.com",
-    'live_test_url': "https://v15demo-int.viindoo.com",
-    'live_test_url_vi_VN': "https://v15demo-vn.viindoo.com",
+    'live_test_url': "https://v14demo-int.viindoo.com",
+    'live_test_url_vi_VN': "https://v14demo-vn.viindoo.com",
     'support': "apps.support@viindoo.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Hidden',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['auth_totp'],
+    'depends': ['product'],
 
     # always loaded
     'data': [
-        'views/user_perferences.xml',
-        'views/templates.xml',
+        'views/res_config_settings_views.xml',
     ],
     'installable': True,
     'application': False,
