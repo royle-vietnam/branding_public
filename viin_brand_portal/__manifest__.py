904e82e383ccd6110135d53c22314496c195ca3d
hung <hungvuhunglinh@gmail.com>
2022-09-21 11:00:16 +0700

    [ADD]viin_brand_portal: Debranding for module Portal

---
+++ viin_brand_portal/__manifest__.py
@@ -0,0 +1,56 @@
+{
+    'name': "Portal Branding For Viindoo",
+    'name_vi_VN': "Giao diện Viindoo cho module Portal",
+
+    'summary': """
+Theme branding Viindoo for module Portal""",
+    'summary_vi_VN': """
+Giao diện brand Viindoo cho module Portal
+""",
+
+    'description': """
+What it does
+============
+This module will change color navigate bar, button and logo,v.v in Portal following Viindoo's brand
+Editions Supported
+==================
+1. Community Edition
+2. Enterprise Edition
+    """,
+
+    'description_vi_VN': """
+Ứng dụng này làm gì
+===================
+Module này sẽ thay đổi giao diện các module Portal theo thương hiệu Viindoo
+Ấn bản được Hỗ trợ
+==================
+1. Ấn bản Community
+2. Ấn bản Enterprise
+""",
+
+    'author': "Viindoo",
+    'website': "https://viindoo.com",
+    'live_test_url': "https://v15demo-int.viindoo.com",
+    'live_test_url_vi_VN': "https://v15demo-vn.viindoo.com",
+    'support': "apps.support@viindoo.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Hidden',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['portal'],
+
+    # always loaded
+    'data': [
+        'views/portal_templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+    'price': 0.0,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+}
