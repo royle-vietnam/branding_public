9007213ac6f978150e7e61fa78f5c1ff6ae18f26
hung <hungvuhunglinh@gmail.com>
2022-09-23 15:07:46 +0700

    [IMP] viin_brand_digest: Branding and add feature archive digest tips

--- viin_brand_digest/__manifest__.py
+++ viin_brand_digest/__manifest__.py
@@ -41,7 +41,7 @@ Module này sẽ thay đổi giao diện module Digest theo thương hiệu Viin
     'support': "apps.support@viindoo.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Hidden',
     'version': '0.1',
@@ -52,6 +52,8 @@ Module này sẽ thay đổi giao diện module Digest theo thương hiệu Viin
     # always loaded
     'data': [
         'data/digest_data.xml',
+        'data/digest_tips_data.xml',
+        'views/digest_views.xml',
     ],
     'installable': True,
     'application': False,
