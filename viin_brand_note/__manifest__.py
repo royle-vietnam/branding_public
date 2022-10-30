36f39abeeaa4199bd698cd8b18b1e6f7b5c6fd15
Cindy Nguyen <kimloan.vnua@gmail.com>
2022-10-30 20:30:25 +0700

    [UPG] viin_brand_note: Upgrade module to 16.0

--- viin_brand_note/__manifest__.py
+++ viin_brand_note/__manifest__.py
@@ -47,9 +47,12 @@ Module này sẽ thay đổi giao diện module Notes theo thương hiệu Viind
     'demo': [
         'data/note_demo.xml',
     ],
-    'installable': False,
+    'data' : [
+        'views/note_demo_views.xml',
+    ],
+    'installable': True,
     'application': False,
-    'auto_install': False, # set True after upgrade 16.0
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
