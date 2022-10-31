96f353dc84b50f41fb3080f55deb62554d56dd8f
Thanhlong2000998 <lethanhlong04012000@gmail.com>
2022-10-31 11:51:47 +0700

    [UPG] viin_brand_website_event: upgrade to 16.0

--- viin_brand_website_event/__manifest__.py
+++ viin_brand_website_event/__manifest__.py
@@ -53,9 +53,9 @@ Module này sẽ thay đổi giao diện module Events theo thương hiệu Viin
     'demo': [
         'data/event_demo.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
-    'auto_install': False, # set True after upgrade 16.0
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
