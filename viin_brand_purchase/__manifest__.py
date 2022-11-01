9ffe194b6ba6728ec13bb185f487dd7aeb031689
Thanhlong2000998 <lethanhlong04012000@gmail.com>
2022-11-01 10:59:51 +0700

    [UPG] viin_brand_purchase: upgrade to 16.0

--- viin_brand_purchase/__manifest__.py
+++ viin_brand_purchase/__manifest__.py
@@ -41,7 +41,7 @@ Module này sẽ thay đổi giao diện các module Purchase theo thương hi�
     'support': "apps.support@viindoo.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Hidden',
     'version': '0.1',
@@ -55,9 +55,9 @@ Module này sẽ thay đổi giao diện các module Purchase theo thương hi�
         'views/res_config_settings_views.xml',
         'views/res_partner_views.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
-    'auto_install': False, # set True after upgrade 16.0
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
