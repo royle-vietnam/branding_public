7ededce03c3a0c14c0fd3c407eb14657ae3603b8
blazewest <daoduyhung98@gmail.com>
2022-11-01 09:59:17 +0700

    [UPG] viin_brand_website_event_exhibitor: update to v16

--- viin_brand_website_event_exhibitor/__manifest__.py
+++ viin_brand_website_event_exhibitor/__manifest__.py
@@ -53,9 +53,9 @@ Module này sẽ thay đổi giao diện module Event Exhibitors theo thương h
     'demo': [
         'data/event_sponsor_demo.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
-    'auto_install': False, # set True after upgrade 16.0
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
