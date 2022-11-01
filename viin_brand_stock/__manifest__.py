783156ba83defca533905a05c6ce5edf5b24e0d3
TrungDuc <dotrungduc.it@gmail.com>
2022-11-01 15:43:27 +0700

    [UPG] viin_brand_stock: upgrade to 16.0

--- viin_brand_stock/__manifest__.py
+++ viin_brand_stock/__manifest__.py
@@ -53,9 +53,9 @@ Module này sẽ thay đổi giao diện các module Stock theo thương hiệu 
     'data': [
         'views/res_config_settings_views.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
-    'auto_install': False, # set True after upgrade 16.0
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
