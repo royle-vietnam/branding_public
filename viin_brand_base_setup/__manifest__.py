cc4a227fbe08f9071b7a05b6145e5db506361259
Thanhlong2000998 <lethanhlong04012000@gmail.com>
2022-11-02 11:19:14 +0700

    [UPG] viin_brand_base_setup: upgrade to 16.0

--- viin_brand_base_setup/__manifest__.py
+++ viin_brand_base_setup/__manifest__.py
@@ -41,7 +41,7 @@ Module này sẽ thay đổi giao diện module Initial Setup Tools theo thươn
     'support': "apps.support@viindoo.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Hidden',
     'version': '0.1',
@@ -53,9 +53,9 @@ Module này sẽ thay đổi giao diện module Initial Setup Tools theo thươn
     'data': [
         'views/res_config_settings_views.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
-    'auto_install': False,  # set True after upgrade to 16.0
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
