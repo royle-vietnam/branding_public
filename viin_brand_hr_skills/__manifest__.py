7779e4a66d8e896bb2424e9929aab8b2c18ae5b4
Trần Thị Mai <59944544+TranThiMAi683@users.noreply.github.com>
2022-12-08 11:47:25 +0700

    [UPG] viin_brand_hr_skills: upg for module to 16.0 (#250)

--- viin_brand_hr_skills/__manifest__.py
+++ viin_brand_hr_skills/__manifest__.py
@@ -43,9 +43,9 @@ Module này sẽ thay đổi giao diện module Skills Management theo thương 
     'data': [
         'views/hr_views.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
-    'auto_install': False, # set True after upgrade 16.0
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
