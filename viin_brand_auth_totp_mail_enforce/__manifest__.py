f39aa88baf68c92322d2753a9880b58e0f5ee740
Thanh Binh <binhhp1702@gmail.com>
2023-02-10 13:11:13 +0700

    [UPG] viin_brand_auth_totp_mail_enforce: upgrade module 15.0 to 16.0

--- viin_brand_auth_totp_mail_enforce/__manifest__.py
+++ viin_brand_auth_totp_mail_enforce/__manifest__.py
@@ -53,9 +53,9 @@ Module này sẽ thay đổi giao diện module Two-Factor Authentication By Mai
     'data': [
         'views/templates.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
-    'auto_install': False,  # set True after upgrade to 16.0
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
