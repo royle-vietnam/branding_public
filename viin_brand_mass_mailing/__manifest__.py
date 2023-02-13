7ef89c7473c7d17378d2e47f951daa42dcf43072
Diamond Nguyen <diamondnguyenkc@gmail.com>
2023-02-13 11:47:01 +0700

    [UPG] viin_brand_mass_mailing: Update v15 to v16

--- viin_brand_mass_mailing/__manifest__.py
+++ viin_brand_mass_mailing/__manifest__.py
@@ -41,7 +41,7 @@ Module này sẽ thay đổi giao diện các module Email Marketing theo thươ
     'support': "apps.support@viindoo.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Hidden',
     'version': '0.1',
@@ -51,16 +51,15 @@ Module này sẽ thay đổi giao diện các module Email Marketing theo thươ
 
     # always loaded
     'data': [
-        'views/mass_mailing_templates_portal.xml',
         'views/mailing_mailing_views.xml',
         'views/mailing_list_views.xml',
     ],
     'demo' : [
         'data/mass_mailing_demo.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
-    'auto_install': False, # set True after upgrade 16.0
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
