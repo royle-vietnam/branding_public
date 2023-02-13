cff330d09605e76ac0bdab96d0ecdf7361bd0061
Diamond Nguyen <diamondnguyenkc@gmail.com>
2023-02-13 08:52:37 +0700

    [UPG] viin_brand_mrp: Update v15 to v16

--- viin_brand_mrp/__manifest__.py
+++ viin_brand_mrp/__manifest__.py
@@ -41,7 +41,7 @@ Module này sẽ thay đổi giao diện các module Manufacturing theo thương
     'support': "apps.support@viindoo.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Hidden',
     'version': '0.1',
@@ -54,9 +54,9 @@ Module này sẽ thay đổi giao diện các module Manufacturing theo thương
         'data/digest_data.xml',
         'views/res_config_settings_views.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
-    'auto_install': False, # set True after upgrade 16.0
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
