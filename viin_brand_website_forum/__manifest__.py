406e26af7fe4a12077e0f09784cff2d8ca18f63c
Tuan Hung <hungvuhunglinh@gmail.com>
2023-02-14 15:59:16 +0700

    [UPG] viin_brand_website_forum: upgrade to v16

--- viin_brand_website_forum/__manifest__.py
+++ viin_brand_website_forum/__manifest__.py
@@ -53,9 +53,9 @@ Module này sẽ thay đổi giao diện module Forum theo thương hiệu Viind
     'demo': [
         'data/forum_demo.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
-    'auto_install': False, # set True after upgrade 16.0
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
