30e48264e67186cd83db5b123bfc46a1b7631175
Thanh Binh <binhhp1702@gmail.com>
2023-02-15 16:40:12 +0700

    [UPG] viin_brand_im_livechat: upgrade module 15.0 to 16.0

--- viin_brand_im_livechat/__manifest__.py
+++ viin_brand_im_livechat/__manifest__.py
@@ -53,9 +53,9 @@ Editions Supported
     'images': [
     	# 'static/description/main_screenshot.png'
     	],
-    'installable': False,
+    'installable': True,
     'application': False,
-    'auto_install': False, # set True after upgrade 16.0
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
