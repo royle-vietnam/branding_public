41c3e86a879341d6cc66e9be5cde282e128c1431
Thanh Binh <binhhp1702@gmail.com>
2023-02-15 14:04:48 +0700

    [UPG] viin_brand_fleet: upgrade module 15.0 to 16.0

--- viin_brand_fleet/__manifest__.py
+++ viin_brand_fleet/__manifest__.py
@@ -48,9 +48,9 @@ Module này sẽ thay đổi giao diện module Fleet theo thương hiệu Viind
         'views/fleet_board_view.xml',
         'views/fleet_vehicle_cost_views.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
-    'auto_install': False, # set True after upgrade 16.0
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
