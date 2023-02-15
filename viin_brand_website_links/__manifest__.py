042ced85e81a4732c83eb3864f300f9077122db0
Tuan Hung <hungvuhunglinh@gmail.com>
2023-02-15 09:01:18 +0700

    [UPG] viin_brand_website_links: upgrade to v16

--- viin_brand_website_links/__manifest__.py
+++ viin_brand_website_links/__manifest__.py
@@ -53,9 +53,9 @@ Module này sẽ thay đổi giao diện module Link Tracker theo thương hiệ
     'data': [
         'views/website_links_template.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
-    'auto_install': False, # set True after upgrade 16.0
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
