54b5d84fbed3cee602b951e5616817498235c84c
Mountain Tran <tranngocson8296@gmail.com>
2023-04-03 17:03:25 +0700

    Revert "[IMP] viin_brand_web: replace color"
    This reverts commit b2ee673455352293651f613b21c0d1a580bd28f3.

--- viin_brand_common/static/src/views/graph/colors.js
+++ viin_brand_common/static/src/views/graph/colors.js
@@ -2,9 +2,9 @@
 
 import * as colors from '@web/views/graph/colors';
 
-var GRAPH_COLORS = ["#00bbce", "#7f4282", "#dd3baf", "#00B365", "#0099E6",
+var GRAPH_COLORS = ["#00bbce", "#7f4282", "#323232", "#00B365", "#0099E6",
     "#FFCC32", "#CC5252", "#ffa100", "#804080", "#343a40",
-    "#66f0ff", "#c796ca", "#ee9dd6", "#59ffb6", "#73d0ff",
+    "#66f0ff", "#c796ca", "#989898", "#59ffb6", "#73d0ff",
     "#ffe598", "#e5a8a8", "#ffd07f", "#ca95ca", "929ca6"];
 
 colors.getColor = function (index, colorScheme) {
