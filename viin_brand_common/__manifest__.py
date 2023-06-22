cb1f827316579314b8aadfb75111921923b86b62
Diamond Nguyen <107665841+diamondnguyenkc@users.noreply.github.com>
2023-06-22 14:58:03 +0700

    [FIX] viin_brand_common: missing whitespace after ',' (#369)

--- viin_brand_common/__manifest__.py
+++ viin_brand_common/__manifest__.py
@@ -47,7 +47,7 @@ Mô đun này thay đổi một vài thông tin dành riêng cho thương hiệu
     ],
     'assets': {
         'web._assets_primary_variables': [
-            ('after', 'web/static/src/scss/primary_variables.scss','viin_brand_common/static/src/scss/primary_variables.scss'),
+            ('after', 'web/static/src/scss/primary_variables.scss', 'viin_brand_common/static/src/scss/primary_variables.scss'),
         ],
         'web._assets_secondary_variables': [
             ('prepend', 'viin_brand_common/static/src/legacy/scss/secondary_variables.scss'),
