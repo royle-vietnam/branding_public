dba853acbe19b09446da9fbacd33d6636431475e
Nguyen Trung Tuan <nguyentrungtuan.hp@gmail.com>
2023-07-28 14:22:29 +0700

    [IMP] viin_brand_pos : override logo of odoo to viindoo
    **Solve the problems**
    - overwrite odoo's logo to viindoo in order screen
    (CustomerFacingDisplayOrder.xml)

--- viin_brand_pos/__manifest__.py
+++ viin_brand_pos/__manifest__.py
@@ -60,6 +60,7 @@ Module này sẽ thay đổi màu sắc của thanh điều hướng (navbar), c
             ('prepend', 'viin_brand_common/static/src/legacy/scss/bootstrap_overridden_common.scss'),
             ('after', 'point_of_sale/static/src/scss/pos.scss', 'viin_brand_pos/static/src/scss/style.scss'),
             'viin_brand_pos/static/src/xml/Chrome.xml',
+            'viin_brand_pos/static/src/xml/CustomerFacingDisplayOrder.xml',
         ],
     },
     'installable': True,
