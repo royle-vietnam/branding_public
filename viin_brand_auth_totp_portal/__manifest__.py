ac2b765ac6be0ba9ad68954d4f9ff757387f9713
Nguyễn Việt Lâm <lamnv0640@gmail.com>
2023-09-23 11:25:11 +0700

    [ADD] viin_brand_auth_totp_portal: add new module (#382)
    * [add] viin_brand_auth_totp_portal: add new module
    
    * Update viin_brand_auth_totp_portal/views/templates.xml
    
    ---------
    
    Co-authored-by: Roy Le <43790414+royleviindoo@users.noreply.github.com>

---
+++ viin_brand_auth_totp_portal/__manifest__.py
@@ -0,0 +1,62 @@
+{
+    'name': "TOTPortal Branding For Viindoo",
+    'name_vi_VN': "Giao diện Viindoo cho module TOTPortal",
+
+    'summary': """
+Theme branding Viindoo for module TOTPortal""",
+    'summary_vi_VN': """
+Giao diện brand Viindoo cho module TOTPortal
+""",
+
+    'description': """
+What it does
+============
+
+This module will change interface in TOTPortal following Viindoo's brand
+
+Editions Supported
+==================
+
+1. Community Edition
+2. Enterprise Edition
+    """,
+
+    'description_vi_VN': """
+Ứng dụng này làm gì
+===================
+
+Module này sẽ thay đổi giao diện module TOTPortal theo thương hiệu Viindoo
+
+Ấn bản được Hỗ trợ
+==================
+
+1. Ấn bản Community
+2. Ấn bản Enterprise
+""",
+
+    'author': "Viindoo",
+    'website': "https://viindoo.com",
+    'live_test_url': "https://v15demo-int.viindoo.com",
+    'live_test_url_vi_VN': "https://v15demo-vn.viindoo.com",
+    'support': "apps.support@viindoo.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Hidden',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['auth_totp_portal'],
+
+    # always loaded
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+    'price': 0.0,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+}
