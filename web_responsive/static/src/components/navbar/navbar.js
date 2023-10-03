3caa2dbcb0e509b0c4852ffd7e4fa22d3348bc1e
Huy Lee <ngochuy97hp@gmail.com>
2023-10-03 16:59:44 +0700

    [MISC] web_responsive: revert apps menu action code

---
+++ web_responsive/static/src/components/navbar/navbar.js
@@ -0,0 +1,19 @@
+/** @odoo-module **/
+
+import {NavBar} from "@web/webclient/navbar/navbar";
+import {useBus, useService} from "@web/core/utils/hooks";
+import {patch} from "web.utils";
+
+const {useRef} = owl;
+
+// Patch Navbar to add proper icon for apps
+patch(NavBar.prototype, "web_responsive.navbar", {
+    setup() {
+        this._super();
+        this.appsMenuService = useService("apps_menu");
+        this.homeMenuButton = useRef("homeMenuButton");
+        useBus(this.env.bus, "TOGGLE_HOME_MENU_BUTTON", ({detail: toggle}) => {
+            this.homeMenuButton.el.classList.toggle('d-none', toggle);
+        });
+    },
+});
