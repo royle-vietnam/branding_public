3ce225aafc573f3cf2bdf2c5206096f7d3702ba8
Huy Lee <ngochuy97hp@gmail.com>
2023-10-06 16:31:20 +0700

    [IMP] web_responsive: export action for ez inherit, fix unnamed action
    Signed-off-by: phamgiang2510

--- web_responsive/static/src/components/apps_menu/apps_menu_service.js
+++ web_responsive/static/src/components/apps_menu/apps_menu_service.js
@@ -13,60 +13,65 @@ export async function nextTick() {
     await new Promise((resolve) => setTimeout(resolve));
 }
 
+export class AppsMenuAction extends Component {
+    setup() {
+        this.menuService = useService("menu");
+        useEffect(() => {
+            this.env.bus.trigger("APPS_MENU:ACT:TOGGLE", true);
+            this.env.bus.trigger("APPS_MENU:TOGGLE", true);
+            document.body.classList.add('o_apps_menu_opened');
+            // first open
+            this.env.bus.trigger('TOGGLE_HOME_MENU_BUTTON', this.env.config.breadcrumbs.length === 1);
+            return () => {
+                document.body.classList.remove('o_apps_menu_opened');
+                this.env.bus.trigger("APPS_MENU:TOGGLE", false);
+                this.env.bus.trigger("APPS_MENU:ACT:TOGGLE", false);
+            }
+        }, () => []);
+    }
+
+    onNavBarDropdownItemSelection(menu) {
+        if (menu) {
+            this.menuService.selectMenu(menu);
+        }
+    }
+
+    getMenuItemHref(payload) {
+        const parts = [`menu_id=${payload.id}`];
+        if (payload.actionID) {
+            parts.push(`action=${payload.actionID}`);
+        }
+        return "#" + parts.join("&");
+    }
+
+    getWebIconData(menu) {
+        let result = "/web_responsive/static/img/default_icon_app.png";
+        if (menu.webIconData) {
+            const prefix = menu.webIconData.startsWith("P")
+                ? "data:image/svg+xml;base64,"
+                : "data:image/png;base64,";
+            result = menu.webIconData.startsWith("data:image")
+                ? menu.webIconData
+                : prefix + menu.webIconData.replace(/\s/g, "");
+        }
+        return result;
+    }
+}
+
+AppsMenuAction.components = {AppsMenu, AppsMenuSearchBar, DropdownItem};
+AppsMenuAction.target = "current";
+AppsMenuAction.template = 'web_responsive.AppsMenuAction';
+
+registry.category("actions").add("apps_menu", AppsMenuAction);
+
+
 export const appsMenuService = {
     dependencies: ["action"],
     start(env) {
         let isOpening = false;
-        class AppsMenuAction extends Component {
-            setup() {
-                this.menuService = useService("menu");
-                useEffect(() => {
-                    isOpening = true;
-                    this.env.bus.trigger("APPS_MENU:TOGGLE", true);
-                    document.body.classList.add('o_apps_menu_opened');
-                    // first open
-                    this.env.bus.trigger('TOGGLE_HOME_MENU_BUTTON', this.env.config.breadcrumbs.length === 1);
-                    return () => {
-                        isOpening = false;
-                        document.body.classList.remove('o_apps_menu_opened');
-                        this.env.bus.trigger("APPS_MENU:TOGGLE", false);
-                    }
-                }, () => []);
-            }
-            onNavBarDropdownItemSelection(menu) {
-                if (menu) {
-                    this.menuService.selectMenu(menu);
-                }
-            }
-
-            getMenuItemHref(payload) {
-                const parts = [`menu_id=${payload.id}`];
-                if (payload.actionID) {
-                    parts.push(`action=${payload.actionID}`);
-                }
-                return "#" + parts.join("&");
-            }
-
-            getWebIconData(menu) {
-                let result = "/web_responsive/static/img/default_icon_app.png";
-                if (menu.webIconData) {
-                    const prefix = menu.webIconData.startsWith("P")
-                        ? "data:image/svg+xml;base64,"
-                        : "data:image/png;base64,";
-                    result = menu.webIconData.startsWith("data:image")
-                        ? menu.webIconData
-                        : prefix + menu.webIconData.replace(/\s/g, "");
-                }
-                return result;
-            }
-        }
-
-        AppsMenuAction.components = {AppsMenu, AppsMenuSearchBar, DropdownItem};
-        AppsMenuAction.target = "current";
-        AppsMenuAction.template = 'web_responsive.AppsMenuAction';
-
-        registry.category("actions").add("apps_menu", AppsMenuAction);
-
+        env.bus.on("APPS_MENU:ACT:TOGGLE", null, toggle => {
+            isOpening = !!toggle;
+        });
         return {
             isOpening() {
                 return isOpening;
@@ -75,7 +80,11 @@ export const appsMenuService = {
                 return new Mutex().exec(async () => {
                     try {
                         if (openState || !isOpening) {
-                            await env.services.action.doAction("apps_menu");
+                            await env.services.action.doAction({
+                                name: env._t("Home Menu"),
+                                type: "ir.actions.client",
+                                tag: "apps_menu",
+                            });
                         } else {
                             await env.services.action.restore();
                         }
