8c29adf3e73d046a04a99cc77d5531ef4680684d
Dang Van Loc <locdangkvh892k@gmail.com>
2023-10-18 16:59:20 +0700

    [IMP] viin_brand_common: imp opacity sample data

--- viin_brand_common/__manifest__.py
+++ viin_brand_common/__manifest__.py
@@ -76,6 +76,7 @@ Mô đun này thay đổi một vài thông tin dành riêng cho thương hiệu
             ('after', 'web/static/src/views/fields/progress_bar/progress_bar_field.scss', 'viin_brand_common/static/src/legacy/scss/progress_bar.scss'),
             ('after', 'web/static/src/views/form/button_box/button_box.scss', 'viin_brand_common/static/src/views/form/button_box/button_box.scss'),
             ('after', 'web/static/src/webclient/webclient.js', 'viin_brand_common/static/src/webclient/webclient.js'),
+            ('after', 'web/static/src/legacy/scss/utils.scss', 'viin_brand_common/static/src/legacy/scss/utils.scss'),
         ],
     },
     'installable': True,
