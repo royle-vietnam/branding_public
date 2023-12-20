e4f0c7edc2baa3ba4bbc57ac3da52b1df11fe9d0
David Tran <david.tran@tvtmarine.com>
2023-12-20 12:04:10 +0700

    [IMP] viin_brand_common: add missing search bar override

--- viin_brand_common/__manifest__.py
+++ viin_brand_common/__manifest__.py
@@ -71,6 +71,7 @@ Mô đun này thay đổi một vài thông tin dành riêng cho thương hiệu
             ('after', 'web/static/src/legacy/scss/form_view_extra.scss', 'viin_brand_common/static/src/legacy/scss/form_view_extra.scss'),
             ('after', 'web/static/src/legacy/scss/kanban_view.scss', 'viin_brand_common/static/src/legacy/scss/kanban_view.scss'),
             ('after', 'web/static/src/search/search_panel/search_view.scss', 'viin_brand_common/static/src/search/search_panel/search_view.scss'),
+            ('after', 'web/static/src/search/search_bar/search_bar.scss', 'viin_brand_common/static/src/search/search_bar/search_bar.scss'),
             ('after', 'base/static/src/scss/onboarding.scss', 'viin_brand_common/static/src/scss/onboarding.scss'),
             ('after', 'web/static/src/webclient/settings_form_view/settings_form_view.scss', 'viin_brand_common/static/src/webclient/settings_form_view/settings_form_view.scss'),
             ('after', 'web/static/src/views/fields/progress_bar/progress_bar_field.scss', 'viin_brand_common/static/src/legacy/scss/progress_bar.scss'),
