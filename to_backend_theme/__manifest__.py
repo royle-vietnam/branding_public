08415f10234f6cd6e5dff092f7c2632b6851ea5a
duongnguyen-viindoo <daiduongnguyen2709@gmail.com>
2024-01-25 14:57:32 +0700

    [IMP] to_backend_theme: improve font weight on pivot view
    Signed-off-by: vietphuong97

--- to_backend_theme/__manifest__.py
+++ to_backend_theme/__manifest__.py
@@ -40,6 +40,7 @@ Backend theme for Viindoo, based on the Openworx Backend Theme
             ('after', '/web_responsive/static/src/legacy/scss/web_responsive.scss', '/to_backend_theme/static/src/legacy/scss/web_responsive.scss'),
             ('after', '/web_responsive/static/src/components/apps_menu/apps_menu.scss', 'to_backend_theme/static/src/scss/apps_menu.scss'),
             'to_backend_theme/static/src/scss/style.scss',
+            ('after', 'web/static/src/views/pivot/pivot_view.scss', 'to_backend_theme/static/src/views/pivot/pivot_view.scss'),
             ],
         },
     'installable': True,
