f54250a3b299137e73454adc4ba127139c718624
duongnguyen-viindoo <daiduongnguyen2709@gmail.com>
2024-03-14 16:11:43 +0700

    [FIX] web_responsive: don't hide attachment preview by default
    -OCA already have discussion at /OCA/web/pull/2695/

--- web_responsive/static/src/views/form/form_controller.esm.js
+++ web_responsive/static/src/views/form/form_controller.esm.js
@@ -8,7 +8,7 @@ import {FormController} from "@web/views/form/form_controller";
 // Patch FormController to always load attachment alongwith the chatter on the side bar
 patch(FormController.prototype, "web_responsive.FormController", {
     setup() {
+        // TODO: remove me in v17/master because viindoo Saas has been suffered errors from some removed JS files in stable version
         this._super();
-        this.hasAttachmentViewerInArch = false;
     },
 });
