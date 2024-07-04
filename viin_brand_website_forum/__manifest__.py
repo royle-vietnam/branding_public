bb9a82f3614a8aff631137371222f7372b1deb44
Soncrits <truongson290893@gmail.com>
2024-07-04 17:08:22 +0700

    [UPG] viin_brand_website_forum: upgrade to 17.0

--- viin_brand_website_forum/__manifest__.py
+++ viin_brand_website_forum/__manifest__.py
@@ -53,7 +53,8 @@ Module này sẽ thay đổi giao diện module Forum theo thương hiệu Viind
     'demo': [
         'data/forum_demo.xml',
     ],
-    'installable': False, # set auto_install True after upgrading for v17 after upgrading for v17
+    'installable': True,
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
