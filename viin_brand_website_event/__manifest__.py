79a6bc4d4693ec11dddd58a7a1e930b4e1a45559
Soncrits <truongson290893@gmail.com>
2024-07-09 14:13:35 +0700

    [UPG] viin_brand_website_event: upgrade to version 17.0

--- viin_brand_website_event/__manifest__.py
+++ viin_brand_website_event/__manifest__.py
@@ -53,7 +53,8 @@ Module này sẽ thay đổi giao diện module Events theo thương hiệu Viin
     'demo': [
         'data/event_demo.xml',
     ],
-    'installable': False, # set auto_install True after upgrading for v17 after upgrading for v17
+    'installable': True,
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
