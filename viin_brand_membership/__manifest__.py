cac29d0f6d622639ecd4b18d1b60062c97e70328
Nguyễn Trung Tuấn <62325566+trungtuan88@users.noreply.github.com>
2024-07-11 13:59:24 +0700

    [UPG] viin_brand_membership: upgrade to v17.0

--- viin_brand_membership/__manifest__.py
+++ viin_brand_membership/__manifest__.py
@@ -30,8 +30,8 @@ Module này sẽ thay đổi giao diện module Members theo thương hiệu Vii
 
     'author': "Viindoo",
     'website': "https://viindoo.com",
-    'live_test_url': "https://v16demo-int.viindoo.com",
-    'live_test_url_vi_VN': "https://v16demo-vn.viindoo.com",
+    'live_test_url': "https://v17demo-int.viindoo.com",
+    'live_test_url_vi_VN': "https://v17demo-vn.viindoo.com",
     'support': "apps.support@viindoo.com",
 
     # Categories can be used to filter modules in modules listing
@@ -47,7 +47,8 @@ Module này sẽ thay đổi giao diện module Members theo thương hiệu Vii
     'data': [
         'views/partner_views.xml',
     ],
-    'installable': False, # set auto_install True after upgrading for v17 after upgrading for v17
+    'installable': True,
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
