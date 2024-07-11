6675581470187c6444daabed66c3f62b2730f2bc
Viinbot <68988903+viinbot@users.noreply.github.com>
2024-07-11 15:19:49 +0700

    [FIX] viin_brand_im_livechat: don't hard code livechat button and header (#487)
    * [FIX] viin_brand_im_livechat: don't hard code livechat button and header
    
    -Problem: User can not config livechat header background color or button
    color because the change in [1] has hard code css color for them
    -Solution: remove the hard code css, move them into default value of
    field definition and writing migration script to upgrade them of course
    
    [1]: https://github.com/Viindoo/branding/commit/40c50dc521f280ba34ff0bbd3a03171a131e125b
    
    Forward-Port-Of: #481

--- viin_brand_im_livechat/__manifest__.py
+++ viin_brand_im_livechat/__manifest__.py
@@ -49,9 +49,7 @@ Editions Supported
         'views/im_livechat_channel_templates.xml',
         'views/im_livechat_channel_views.xml'
     ],
-    'images': [
-        # 'static/description/main_screenshot.png'
-        ],
+    'post_init_hook': 'post_init_hook',
     'installable': True,
     'application': False,
     'auto_install': True,
