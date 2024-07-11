2485bfa3f2887fe877c0f4fbe42bc810431a4426
Nguyễn Trung Tuấn <62325566+trungtuan88@users.noreply.github.com>
2024-07-11 15:32:44 +0700

    [UPG] viin_brand_social_media: upgrade to 17.0 (#498)

--- viin_brand_social_media/__manifest__.py
+++ viin_brand_social_media/__manifest__.py
@@ -34,8 +34,8 @@ Module này sẽ thay đổi giao diện các module Social Media theo thương 
 
     'author': "Viindoo",
     'website': "https://viindoo.com",
-    'live_test_url': "https://v16demo-int.viindoo.com",
-    'live_test_url_vi_VN': "https://v16demo-vn.viindoo.com",
+    'live_test_url': "https://v17demo-int.viindoo.com",
+    'live_test_url_vi_VN': "https://v17demo-vn.viindoo.com",
     'support': "apps.support@viindoo.com",
 
     # Categories can be used to filter modules in modules listing
@@ -51,7 +51,8 @@ Module này sẽ thay đổi giao diện các module Social Media theo thương 
     'demo': [
         'demo/res_company_demo.xml',
     ],
-    'installable': False, # set auto_install True after upgrading for v17 after upgrading for v17
+    'installable': True,
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
