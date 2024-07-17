130304133460a3d5c1ef2b37c8dcd146e08a9e7c
Soncrits <truongson290893@gmail.com>
2024-07-17 10:28:08 +0700

    [UPG] viin_brand_mass_mailing: upgrade version 17.0
    - edit res_config_setting view, xpath to setting instead of div
    - Remove duplicate act window action records

--- viin_brand_mass_mailing/__manifest__.py
+++ viin_brand_mass_mailing/__manifest__.py
@@ -63,7 +63,8 @@ Module này sẽ thay đổi giao diện các module Email Marketing theo thươ
     'demo': [
         'data/mass_mailing_demo.xml',
     ],
-    'installable': False, # set auto_install True after upgrading for v17 after upgrading for v17
+    'installable': True,
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
