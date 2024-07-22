33980778dce5a99d2d851ec9027a5cb4b9268fbb
Soncrits <truongson290893@gmail.com>
2024-07-22 14:14:35 +0700

    [UPG] viin_brand_mass_mailing_themes: upgrade to 17.0

--- viin_brand_mass_mailing_themes/__manifest__.py
+++ viin_brand_mass_mailing_themes/__manifest__.py
@@ -42,7 +42,8 @@ Module này sẽ thay đổi giao diện module Mass Mailing Themes theo thươn
     'data': [
         'views/mass_mailing_themes_templates.xml',
     ],
-    'installable': False, # set auto_install True after upgrading for v17 after upgrading for v17
+    'installable': True,
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
