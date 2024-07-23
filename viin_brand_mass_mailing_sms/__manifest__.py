4ca5b17b3e05aab656a1ddf4b2c8bbbce501614b
Soncrits <truongson290893@gmail.com>
2024-07-23 15:57:22 +0700

    [UPG] viin_brand_mass_mailing_sms: upgrade to version 17.0

--- viin_brand_mass_mailing_sms/__manifest__.py
+++ viin_brand_mass_mailing_sms/__manifest__.py
@@ -53,7 +53,8 @@ Module này sẽ thay đổi giao diện các module SMS Marketing theo thương
     'data': [
         'views/mailing_list_views.xml',
     ],
-    'installable': False, # set auto_install True after upgrading for v17 after upgrading for v17
+    'installable': True,
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
