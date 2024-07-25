ee498bf8be805fc7cca740f4bd5bf3feb61966af
Soncrits <truongson290893@gmail.com>
2024-07-25 13:19:43 +0700

    [UPG] viin_brand_auth_totp_mail_enforce: upgrade to version 17.0

--- viin_brand_auth_totp_mail_enforce/__manifest__.py
+++ viin_brand_auth_totp_mail_enforce/__manifest__.py
@@ -53,7 +53,8 @@ Module này sẽ thay đổi giao diện module Two-Factor Authentication By Mai
     'data': [
         'views/templates.xml',
     ],
-    'installable': False, # set auto_install True after upgrading for v17 after upgrading for v17
+    'installable': True,
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
