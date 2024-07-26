37d57a372a34095053ccd8974632aba0923ba9b9
Soncrits <truongson290893@gmail.com>
2024-07-26 11:43:52 +0700

    [UPG] viin_brand_fleet: upgrade to version 17.0

--- viin_brand_fleet/__manifest__.py
+++ viin_brand_fleet/__manifest__.py
@@ -48,7 +48,8 @@ Module này sẽ thay đổi giao diện module Fleet theo thương hiệu Viind
         'views/fleet_board_view.xml',
         'views/fleet_vehicle_cost_views.xml',
     ],
-    'installable': False, # set auto_install True after upgrading for v17 after upgrading for v17
+    'installable': True,
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
