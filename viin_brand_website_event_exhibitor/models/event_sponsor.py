eacb094e734cf728ad26954ae521668f0f6fb3f2
Soncrits <truongson290893@gmail.com>
2024-07-26 14:06:01 +0700

    [IMP] viin_brand_website_event_exhibitor: improve onchange function

--- viin_brand_website_event_exhibitor/models/event_sponsor.py
+++ viin_brand_website_event_exhibitor/models/event_sponsor.py
@@ -6,11 +6,7 @@ class Sponsor(models.Model):
 
     @api.onchange('exhibitor_type')
     def _onchange_exhibitor_type(self):
-        for sponsor in self:
-            if sponsor.exhibitor_type == 'online' and not sponsor.room_name:
-                if sponsor.name:
-                    room_name = "viindoo-exhibitor-%s" % sponsor.name
-                else:
-                    room_name = self.env['chat.room']._default_name(objname='exhibitor')
-                sponsor.room_name = self._jitsi_sanitize_name(room_name)
         super()._onchange_exhibitor_type()
+        for r in self:
+            if 'odoo-' in r.room_name:
+                r.room_name = r.room_name.replace('odoo-', 'viindoo-')
