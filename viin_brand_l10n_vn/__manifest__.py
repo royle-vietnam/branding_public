cb63cbc90d7f84a5268b65f005b18a185dd14091
TuyenTran <trandinhtuyen2018@gmail.com>
2025-06-05 10:47:41 +0700

    [ADD] viin_brand_l10n_vn: Change document url from Odoo to Viindoo
    **Reason**
    - Document url is not Viindoo
    
    **Result**
    - Add a new module in branding to change url

---
+++ viin_brand_l10n_vn/__manifest__.py
@@ -0,0 +1,48 @@
+{
+    'name': "VietNam Localization Debranding for Viindoo",
+    'name_vi_VN': "Bản địa hóa(Việt Nam) Thương hiệu Viindoo",
+
+    'summary': """
+Debranding VietNam Localization for Viindoo
+    """,
+    'summary_vi_VN': """
+Bản địa hóa Việt Nam theo Thương hiệu Viindoo
+    """,
+
+    'description': """
+
+Editions Supported
+==================
+1. Community Edition
+2. Enterprise Edition
+
+    """,
+
+    'description_vi_VN': """
+
+Ấn bản được Hỗ trợ
+==================
+1. Ấn bản Community
+2. Ấn bản Enterprise
+
+    """,
+
+    'author': "Viindoo",
+    'website': "https://viindoo.com",
+    'live_test_url': "https://v17demo-int.viindoo.com",
+    'live_test_url_vi_VN': "https://v17demo-vn.viindoo.com",
+    'support': "apps.support@viindoo.com",
+    'category': 'Hidden',
+    'version': '0.1',
+
+    'depends': ['l10n_vn'],
+
+    'data': [
+        'views/res_bank_views.xml'
+    ],
+    'auto_install': True,
+    'installable': True,
+    'price': 0.0,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+}
