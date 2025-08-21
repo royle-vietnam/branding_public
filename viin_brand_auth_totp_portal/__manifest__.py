c6fc7dc0c2e3edd6cb1b1efae815c8b8e1bd7910
Roy Le <roy.le@viindoo.com>
2025-08-21 13:15:46 +0700

    [FIX] web_responsive: fix pylint

--- viin_brand_auth_totp_portal/__manifest__.py
+++ viin_brand_auth_totp_portal/__manifest__.py
@@ -36,8 +36,8 @@ Module này sẽ thay đổi giao diện module TOTPortal theo thương hiệu V
 
     'author': "Viindoo",
     'website': "https://viindoo.com",
-    'live_test_url': "https://v16demo-int.viindoo.com",
-    'live_test_url_vi_VN': "https://v16demo-vn.viindoo.com",
+    'live_test_url': "https://v18demo-int.viindoo.com",
+    'live_test_url_vi_VN': "https://v18demo-vn.viindoo.com",
     'support': "apps.support@viindoo.com",
 
     # Categories can be used to filter modules in modules listing
@@ -53,7 +53,7 @@ Module này sẽ thay đổi giao diện module TOTPortal theo thương hiệu V
     'data': [
         'views/templates.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
