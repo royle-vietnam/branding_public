2adea66c4ef1f532cf2a87dd9f87a1b80a69fe38
TuyenTran <trandinhtuyen2018@gmail.com>
2025-09-10 15:45:14 +0700

    [UPG] viin_brand_auth_oauth: no change

--- viin_brand_auth_oauth/__manifest__.py
+++ viin_brand_auth_oauth/__manifest__.py
@@ -36,8 +36,8 @@ Module này sẽ thay đổi giao diện module OAuth2 Authentication theo thư�
 
     'author': "Viindoo",
     'website': "https://viindoo.com",
-    'live_test_url': "https://v17demo-int.viindoo.com",
-    'live_test_url_vi_VN': "https://v17demo-vn.viindoo.com",
+    'live_test_url': "https://v18demo-int.viindoo.com",
+    'live_test_url_vi_VN': "https://v18demo-vn.viindoo.com",
     'support': "apps.support@viindoo.com",
 
     # Categories can be used to filter modules in modules listing
@@ -53,7 +53,7 @@ Module này sẽ thay đổi giao diện module OAuth2 Authentication theo thư�
     'data': [
         'data/auth_oauth_data.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
