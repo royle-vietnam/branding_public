72ec7c449090ba09b186d66362aaeab52cc550cb
TuyenTran <trandinhtuyen2018@gmail.com>
2025-09-12 13:13:11 +0700

    [UPG] viiin_brand_calendar: export translations

--- viin_brand_calendar/__manifest__.py
+++ viin_brand_calendar/__manifest__.py
@@ -47,7 +47,7 @@ Module này sẽ thay đổi giao diện module Calendar theo thương hiệu Vi
     'data': [
         'views/calendar_views.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
