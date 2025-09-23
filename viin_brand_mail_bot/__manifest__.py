48948a68f46ec3ee9d41916af829b253ae63a1aa
TuyenTran <trandinhtuyen2018@gmail.com>
2025-09-23 09:16:33 +0700

    [UPG] viin_brand_mail_bot: export translation

--- viin_brand_mail_bot/__manifest__.py
+++ viin_brand_mail_bot/__manifest__.py
@@ -35,15 +35,15 @@ Module này sẽ thay đổi giao diện module Mail Bot theo thương hiệu Vi
 
     'author': "Viindoo",
     'website': "https://viindoo.com",
-    'live_test_url': "https://v16demo-int.viindoo.com",
-    'live_test_url_vi_VN': "https://v16demo-vn.viindoo.com",
+    'live_test_url': "https://v18demo-int.viindoo.com",
+    'live_test_url_vi_VN': "https://v18demo-vn.viindoo.com",
     'support': "apps.support@viindoo.com",
     'category': 'Hidden',
     'version': '0.1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['mail_bot', 'viin_brand_common'],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
