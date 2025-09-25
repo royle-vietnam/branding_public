ccc9b08059692e36233bed50997e124a5da6596d
TuyenTran <trandinhtuyen2018@gmail.com>
2025-09-25 09:48:38 +0700

    [UPG] viin_brand_pos: upgrade 18*
    - Export translation
    - Fix js because  odoo merged files
    - Remove outdated views and xpath

--- viin_brand_pos/__manifest__.py
+++ viin_brand_pos/__manifest__.py
@@ -36,8 +36,8 @@ Module này sẽ thay đổi màu sắc của thanh điều hướng (navbar), c
 
     'author': "Viindoo",
     'website': "https://viindoo.com",
-    'live_test_url': "https://v16demo-int.viindoo.com",
-    'live_test_url_vi_VN': "https://v16demo-vn.viindoo.com",
+    'live_test_url': "https://v18demo-int.viindoo.com",
+    'live_test_url_vi_VN': "https://v18demo-vn.viindoo.com",
     'support': "apps.support@viindoo.com",
 
     # Categories can be used to filter modules in modules listing
@@ -58,17 +58,16 @@ Module này sẽ thay đổi màu sắc của thanh điều hướng (navbar), c
             ('prepend', 'viin_brand_common/static/src/scss/primary_variables.scss'),
             ('prepend', 'viin_brand_common/static/src/legacy/scss/bootstrap_overridden_common.scss'),
             ('after', 'point_of_sale/static/src/scss/pos.scss', 'viin_brand_pos/static/src/scss/style.scss'),
-            'viin_brand_pos/static/src/xml/CustomerFacingDisplayOrder.xml',
             'viin_brand_pos/static/src/app/screens/receipt/order_receipt.xml',
             'viin_brand_pos/static/src/app/navbar/navbar.xml',
             'viin_brand_pos/static/src/app/navbar/navbar.js',
-            'viin_brand_pos/static/src/app/popups/offline_error_popup.js',
+            ('after', 'point_of_sale/static/src/app/errors/error_handlers.js', 'viin_brand_pos/static/src/app/popups/offline_error_popup.js'),
         ],
         'point_of_sale.assets_prod': [
             'viin_brand_pos/static/src/css/**/*',
         ],
     },
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
--- viin_brand_pos/static/src/app/popups/offline_error_popup.js
+++ viin_brand_pos/static/src/app/popups/offline_error_popup.js
@@ -1,8 +1,25 @@
 /** @odoo-module */
-
-import { OfflineErrorPopup } from "@point_of_sale/app/errors/popups/offline_error_popup";
+import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
+import { ConnectionLostError } from "@web/core/network/rpc";
+import { registry } from "@web/core/registry";
 import { _t } from "@web/core/l10n/translation";
 
-OfflineErrorPopup.defaultProps.body = _t(
-    "Meanwhile connection is back, Viindoo Point of Sale will operate limited operations. Check your connection or continue with limited functionalities"
-);
+
+function offlineErrorHandlerOverride(env, error, originalError) {
+    if (originalError instanceof ConnectionLostError) {
+        if (!env.services.pos.data.network.warningTriggered) {
+            env.services.dialog.add(AlertDialog, {
+                title: _t("Connection Lost"),
+                body: _t(
+                    "Meanwhile connection is back, Viindoo Point of Sale will operate limited operations. Check your connection or continue with limited functionalities"
+                ),
+                confirmLabel: _t("Continue with limited functionality"),
+            });
+            env.services.pos.data.network.warningTriggered = true;
+        }
+
+        return true;
+    }
+}
+
+registry.category("error_handlers").add("offlineErrorHandler", offlineErrorHandlerOverride, { force: true });
