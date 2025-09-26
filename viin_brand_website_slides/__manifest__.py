47b9d42940a2b2b68a7ce9326e31093c3d921859
TuyenTran <trandinhtuyen2018@gmail.com>
2025-09-26 16:39:09 +0700

    [UPG] viin_brand_website_slide: export translation

--- viin_brand_website_slides/__manifest__.py
+++ viin_brand_website_slides/__manifest__.py
@@ -27,8 +27,8 @@ Editions Supported
 
     'author': "Viindoo",
     'website': "https://viindoo.com",
-    'live_test_url': "https://v16demo-int.viindoo.com",
-    'live_test_url_vi_VN': "https://v16demo-vn.viindoo.com",
+    'live_test_url': "https://v18demo-int.viindoo.com",
+    'live_test_url_vi_VN': "https://v18demo-vn.viindoo.com",
     'support': "apps.support@viindoo.com",
 
     # Categories can be used to filter modules in modules listing
@@ -43,7 +43,7 @@ Editions Supported
     'images': [
         # 'static/description/main_screenshot.png'
         ],
-    'installable': False,
+    'installable': True,
     'auto_install': ['website_slides'],
     'price': 0.0,
     'currency': 'EUR',
