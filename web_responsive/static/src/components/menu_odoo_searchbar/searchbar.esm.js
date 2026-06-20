9530b0a4b05f8072b5e2e4b177349c94df4486c0
Roy Le <roy.le@viindoo.com>
2026-06-20 08:20:10 +0700

    [FIX] web_responsive: converting OWL component props/template to static class

--- web_responsive/static/src/components/menu_odoo_searchbar/searchbar.esm.js
+++ web_responsive/static/src/components/menu_odoo_searchbar/searchbar.esm.js
@@ -12,6 +12,9 @@ import {useAutofocus, useService} from "@web/core/utils/hooks";
  * @property {{el: HTMLInputElement}} searchBarInput
  */
 export class AppsMenuOdooSearchBar extends Component {
+    static props = {};
+    static template = "web_responsive.AppsMenuOdooSearchBar";
+
     setup() {
         super.setup();
         this.state = useState({
@@ -59,6 +62,3 @@ export class AppsMenuOdooSearchBar extends Component {
         this.command.openMainPalette({searchValue}, null);
     }
 }
-
-AppsMenuOdooSearchBar.props = {};
-AppsMenuOdooSearchBar.template = "web_responsive.AppsMenuOdooSearchBar";
