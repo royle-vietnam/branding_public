76cf26975a402fac24c71525ae1f1aa6e6b599a6
David Tran <david.tran@tvtmarine.com>
2026-08-08 07:49:01 +0700

    [UPG] viin_brand_website_links: mark installable + upgrade to 19.0
    Signed-off-by: David Tran <david.tran@tvtmarine.com>

--- viin_brand_website_links/__manifest__.py
+++ viin_brand_website_links/__manifest__.py
@@ -53,7 +53,7 @@ Module này sẽ thay đổi giao diện module Link Tracker theo thương hiệ
     'data': [
         'views/website_links_template.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
