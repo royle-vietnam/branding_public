d53c44bc1456b4bc551a67557ada97205b506160
David Tran <david.tran@tvtmarine.com>
2026-08-08 07:49:01 +0700

    [UPG] viin_brand_fleet: mark installable + upgrade to 19.0
    Signed-off-by: David Tran <david.tran@tvtmarine.com>

--- viin_brand_fleet/__manifest__.py
+++ viin_brand_fleet/__manifest__.py
@@ -50,7 +50,7 @@ Module này sẽ thay đổi giao diện module Fleet theo thương hiệu Viind
         'views/fleet_board_view.xml',
         'views/fleet_vehicle_cost_views.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
